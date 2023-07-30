af8c224ce9b6088ed5fbb45e1f1f410f315332fb
mina <sadeghbayan.mina@gmail.com>
2023-07-30 21:46:27 +0330

    fetch users added to Users component

--- src/user/pages/Users.jsx
+++ src/user/pages/Users.jsx
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useHttpClient } from "../../shared/hooks/http-hook";
+import ErrorModal from "../../shared/components/UIElements/ErrorModal";
+import LoadingSpinner from "../../shared/components/UIElements/LoadingSpinner";
+import UserList from "../components/UserList";
 
 const Users = () => {
-  return <div></div>;
+  const { isLoading, error, sendRequest, clearError } = useHttpClient();
+  const [loadedUsers, setLoadedUsers] = useState();
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const responseData = await sendRequest(
+          "http://localhost:5000/api/users"
+        );
+
+        setLoadedUsers(responseData.users);
+      } catch (err) {}
+    };
+    fetchUsers();
+  }, [sendRequest]);
+
+  return (
+    <React.Fragment>
+      <ErrorModal show={error} onClear={clearError} />
+      {isLoading && <LoadingSpinner />}
+      {!isLoading && loadedUsers && <UserList users={loadedUsers} />}
+    </React.Fragment>
+  );
 };
 
 export default Users;
