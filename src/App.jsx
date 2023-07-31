bd2e24e91e68801da4f644927c20ad1f85327238
mina <sadeghbayan.mina@gmail.com>
2023-07-31 16:42:05 +0330

    newPlace and userPlaces added to routes

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import Auth from "./user/pages/Auth";
 import Users from "./user/pages/Users";
 import { useAuth } from "./shared/hooks/auth-hook";
 import { AuthContext } from "./shared/context/auth-context";
+import UserPlaces from "./place/pages/UserPlaces";
+import NewPlace from "./place/pages/NewPlace";
 
 function App() {
   const { token, login, logout, userId } = useAuth();
@@ -17,6 +19,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Users />} />
         <Route path="/auth" element={<Auth />} />
+        <Route path="/:userId/places" element={<UserPlaces />} />
+        <Route path="/places/new" element={<NewPlace />} />
       </Routes>
     );
   } else {
@@ -24,6 +28,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Users />} />
         <Route path="/auth" element={<Auth />} />
+        <Route path="/:userId/places" element={<UserPlaces />} />
+        <Route path="/places/new" element={<NewPlace />} />
       </Routes>
     );
   }
