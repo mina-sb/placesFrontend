840f9c57699c7f39458d5a599b84257784587d35
mina <sadeghbayan.mina@gmail.com>
2023-08-01 11:59:48 +0330

    some of http-hook codes temproly commented

--- src/shared/hooks/http-hook.jsx
+++ src/shared/hooks/http-hook.jsx
@@ -9,22 +9,22 @@ export const useHttpClient = () => {
   const sendRequest = useCallback(
     async (url, method = "GET", body = null, headers = {}) => {
       setIsLoading(true);
-      const httpAbortCtrl = new AbortController();
-      activeHttpRequests.current.push(httpAbortCtrl);
+      //const httpAbortCtrl = new AbortController();
+      //activeHttpRequests.current.push(httpAbortCtrl);
 
       try {
         const response = await fetch(url, {
           method,
           body,
           headers,
-          signal: httpAbortCtrl.signal,
+          //signal: httpAbortCtrl.signal,
         });
 
         const responseData = await response.json();
 
-        activeHttpRequests.current = activeHttpRequests.current.filter(
-          (reqCtrl) => reqCtrl !== httpAbortCtrl
-        );
+        //activeHttpRequests.current = activeHttpRequests.current.filter(
+        //(reqCtrl) => reqCtrl !== httpAbortCtrl
+        //);
 
         if (!response.ok) {
           throw new Error(responseData.message);
@@ -48,7 +48,7 @@ export const useHttpClient = () => {
   useEffect(() => {
     return () => {
       // eslint-disable-next-line react-hooks/exhaustive-deps
-      activeHttpRequests.current.forEach((abortCtrl) => abortCtrl.abort());
+      //activeHttpRequests.current.forEach((abortCtrl) => abortCtrl.abort());
     };
   }, []);
 
