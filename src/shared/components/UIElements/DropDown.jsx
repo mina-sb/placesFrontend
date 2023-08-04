ca5204128ee4f4d3ae394049b4866ca101729aca
mina <sadeghbayan.mina@gmail.com>
2023-08-04 11:23:50 +0330

    goToProfile added to dropdown

--- src/shared/components/UIElements/DropDown.jsx
+++ src/shared/components/UIElements/DropDown.jsx
@@ -1,24 +1,44 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import "./DropDown.css";
 import { AuthContext } from "../../context/auth-context";
+import { useNavigate } from "react-router-dom";
 
 const DropDown = (props) => {
   const auth = useContext(AuthContext);
+  const navigate = useNavigate();
 
   const closeMenu = (e) => {
+    if (e.target.id === "profile") {
+      navigate(`/${auth.userId}/places`);
+      document.removeEventListener("mousedown", closeMenu);
+    }
     if (e.target.id === "logout") {
       auth.logout();
+      document.removeEventListener("mousedown", closeMenu);
     } else {
       props.close();
     }
   };
-  document.addEventListener("mousedown", closeMenu);
+  useEffect(() => {
+    if (props.show) {
+      document.addEventListener("mousedown", closeMenu);
+    }
+  }, [props.show]);
 
   const dropdown = (
     <ul className="dropdown-list">
       <li></li>
-      <li></li>
-      <li id="logout">Logout</li>
+      <li>
+        <div id="profile">
+          <i class="bx bxs-user-rectangle"></i> My Profile
+        </div>
+      </li>
+      <li>
+        <div id="logout">
+          <i class="bx bxs-exit"></i>
+          Logout
+        </div>
+      </li>
     </ul>
   );
   return <div className="dropdown">{props.show ? dropdown : ""}</div>;
