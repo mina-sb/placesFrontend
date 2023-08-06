ba6c2da5d22dc348bdf838d187017066d20789c1
mina <sadeghbayan.mina@gmail.com>
2023-08-06 08:45:02 +0330

    places component implemented

--- src/Main.jsx
+++ src/Main.jsx
@@ -3,12 +3,14 @@ import Users from "./user/pages/Users";
 import AboveTheFold from "./AboveTheFold";
 
 import styles from "./Main.module.css";
+import Places from "./place/components/Places";
 
 const Main = () => {
   return (
     <React.Fragment>
       <AboveTheFold />
       <Users />
+      <Places />
     </React.Fragment>
   );
 };
---
+++ src/place/components/Places.jsx
@@ -0,0 +1,43 @@
+import React, { useEffect, useState } from "react";
+import { useHttpClient } from "../../shared/hooks/http-hook";
+import ErrorModal from "../../shared/components/UIElements/ErrorModal";
+import LoadingSpinner from "../../shared/components/UIElements/LoadingSpinner";
+import PlaceList from "../components/PlaceList";
+import styles from "./Places.module.css";
+
+const Places = () => {
+  const { isLoading, error, sendRequest, clearError } = useHttpClient();
+  const [loadedPlaces, setLoadedPlaces] = useState();
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const responseData = await sendRequest(
+          "http://localhost:5000/api/places"
+        );
+
+        setLoadedPlaces(responseData.places);
+      } catch (err) {}
+    };
+    fetchUsers();
+  }, [sendRequest]);
+
+  return (
+    <React.Fragment>
+      <ErrorModal show={error} onClear={clearError} />
+      {isLoading && (
+        <div className="center">
+          <LoadingSpinner />
+        </div>
+      )}
+      {!isLoading && loadedPlaces && (
+        <div className={styles.places}>
+          <h2 className={styles.places_h2}>Places</h2>
+          <PlaceList items={loadedPlaces} />
+        </div>
+      )}
+    </React.Fragment>
+  );
+};
+
+export default Places;
