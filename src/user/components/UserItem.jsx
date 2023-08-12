dd4136034d3bf4945389829f1df1bb7f406e0d18
mina <sadeghbayan.mina@gmail.com>
2023-08-12 08:52:38 +0330

    userItem fixed for when userImg doesnt exist

--- src/user/components/UserItem.jsx
+++ src/user/components/UserItem.jsx
@@ -1,19 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import Card from "../../shared/components/UIElements/Card";
 import "./UserItem.css";
 import Avatar from "../../shared/components/UIElements/Avatar";
+import defaultImg from "../../shared/assets/6369737.png";
 
 const UserItem = (props) => {
+  const [img, setImg] = useState("");
+  useEffect(() => {
+    if (props.image) {
+      setImg(`http://localhost:5000/${props.image}`);
+    } else {
+      setImg(defaultImg);
+    }
+  }, []);
+
   return (
     <li className="user-item">
       <Card className="user-item__content">
         <Link to={`/${props.id}/places`}>
           <div className="user-item__image">
-            <Avatar
-              image={`http://localhost:5000/${props.image}`}
-              alt={props.name}
-            />
+            <Avatar image={img} alt={props.name} />
           </div>
           <div className="user-item__info">
             <h2>{props.name}</h2>
