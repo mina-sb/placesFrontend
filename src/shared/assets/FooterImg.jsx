52f5eebc661024fbdf1c9875a2cc9ddc87932490
mina <sadeghbayan.mina@gmail.com>
2023-08-16 11:52:15 +0330

    minor fixes in placeList and footer

--- src/shared/assets/FooterImg.jsx
+++ src/shared/assets/FooterImg.jsx
@@ -7,6 +7,7 @@ const FooterImg = () => {
         width="493"
         height="346"
         viewBox="0 0 493 346"
+        className="dd"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
       >
