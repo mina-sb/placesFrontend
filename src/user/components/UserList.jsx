f46ebc37c49925a1a1a0e3d9eaee6fddbcd1a405
mina <sadeghbayan.mina@gmail.com>
2023-08-16 07:48:48 +0330

    swiper added to userList

--- src/user/components/UserList.jsx
+++ src/user/components/UserList.jsx
@@ -1,23 +1,55 @@
 import React from "react";
 import UserItem from "./UserItem";
+
 import "./UserList.css";
+import { Navigation, Pagination } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/pagination";
+import { Swiper, SwiperSlide } from "swiper/react";
 
 const UserList = (props) => {
+  const [swiper, setSwiper] = React.useState(null);
+
+  const nexto = () => {
+    swiper.slideNext();
+  };
+  const prevto = () => {
+    swiper.slidePrev();
+  };
   if (props.users.length === 0) {
     return <h2>There is no user yet!</h2>;
   } else {
     return (
-      <ul className="users-list">
-        {props.users.map((user) => (
-          <UserItem
-            key={user.id}
-            id={user.id}
-            image={user.image}
-            name={user.name}
-            placeCount={user.places.length}
-          />
-        ))}
-      </ul>
+      <React.Fragment>
+        <Swiper
+          modules={[Navigation, Pagination]}
+          spaceBetween={22}
+          onSwiper={(e) => {
+            setSwiper(e);
+          }}
+          slidesPerView="auto"
+        >
+          {props.users.map((user) => (
+            <SwiperSlide className="res-slide">
+              <UserItem
+                key={user.id}
+                id={user.id}
+                image={user.image}
+                name={user.name}
+                placeCount={user.places.length}
+              />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+        <div className="swiper-nav-btns">
+          <button onClick={prevto}>
+            <i class="bx bxs-chevron-left mt"></i>
+          </button>
+          <button onClick={nexto}>
+            <i class="bx bxs-chevron-right mt"></i>
+          </button>
+        </div>
+      </React.Fragment>
     );
   }
 };
